9edce8a8c7e1a265e07c8eea88f8f7c90fca7ec3
Woodhouse <pariswoodhouse@hotmail.com>
2021-12-07 21:00:31 -0800

    Function to make random selection

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('hi')
+function computerPlay() {
+  let selection = Math.ceil(Math.random() * 3);
+
+  if (selection === 1) {
+    console.log('Rock');
+  } else if (selection === 2) {
+    console.log('Paper');
+  } else if (selection === 3) {
+    console.log('Scissors');
+  }
+}
+
+
